b707d029ec8b99d8fa74392e278164ba85fe67c4
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-10-12 10:59:30 -0500

    IInitial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 4400;
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
+app.listen(port,()=> console.log(`server running on port ${port}!`))
