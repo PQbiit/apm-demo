c8a3b85c874a3f76e245086a721701afc2cd773a
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-10-12 11:14:59 -0500

    Implemented rollback

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const path = require('path');
+const rollbar = new Rollbar({
+    accesstoken: 'bc1922e0127d4050bbf0f0027cc3762f',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
 
 const app = express();
 const port = process.env.PORT || 4400;
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname,'/public/index.html'));
+    rollbar.info('HTML file served successfully');
 });
 
 app.listen(port,()=> console.log(`server running on port ${port}!`))
